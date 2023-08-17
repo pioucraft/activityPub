2129d3b6ac5c8506956bcaaad01b0c8ddf682f34
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-17 19:04:09 +0200

    testing

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express"
 import cors from "cors"
+import bodyParser from "body-parser"
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(bodyParser.json())
 
 
 
