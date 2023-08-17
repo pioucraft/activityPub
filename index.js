6a5cb6bb984b380fae9099172374e129ade160ab
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-17 19:10:54 +0200

    testing

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
 
+const activityJsonMiddleware = bodyParser.json({ type: 'activity+json' });
 const app = express()
 app.use(cors())
-app.use(bodyParser.json())
+app.use(activityJsonMiddleware())
 
 
 
