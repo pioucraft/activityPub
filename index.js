a15957151518a03e66636fea70f6abb0e282555a
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-18 08:54:19 +0200

    testing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import crypo from "node:crypto"
 import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
+
 const app = express()
 app.use(cors())
 
@@ -22,16 +24,57 @@ app.all("/actor", (req, res) => {
 })
 
 
-app.all("/inbox", (req, res) => {
+app.all("/inbox", async (req, res) => {
     console.log(req)
-    let follower = req.body.actor
-    const followAccept = {
-      "@context": "https://www.w3.org/ns/activitystreams",
-      type: "Follow",
-      actor: "https://social.gougoule.ch/actor",
-      object: follower,
-    };
-    res.json(followAccept)
+    let actor = req.body.actor
+    await send(req.app.get("actor"), req.body.actor, {
+        "@context": "https://www.w3.org/ns/activitystreams",
+        id: `https://social.gougoule.ch/${crypto.randomUUID()}`,
+        type: "Accept",
+        actor,
+        object: body,
+      });
+
 })
+
+async function send(sender: string, recipient: string, message: object) {
+  const url = new URL(recipient);
+
+  const actor = await fetchActor(recipient);
+  const fragment = actor.inbox.replace("https://" + url.hostname, "");
+  const body = JSON.stringify(message);
+  const digest = crypto.createHash("sha256").update(body).digest("base64");
+  const d = new Date();
+
+  const key = crypto.createPrivateKey(PRIVATE_KEY.toString());
+  const data = [
+    `(request-target): post ${fragment}`,
+    `host: ${url.hostname}`,
+    `date: ${d.toUTCString()}`,
+    `digest: SHA-256=${digest}`,
+  ].join("\n");
+  const signature = crypto
+    .sign("sha256", Buffer.from(data), key)
+    .toString("base64");
+
+  const res = await fetch(actor.inbox, {
+    method: "POST",
+    headers: {
+      host: url.hostname,
+      date: d.toUTCString(),
+      digest: `SHA-256=${digest}`,
+      "content-type": "application/json",
+      signature: `keyId="${sender}#main-key",headers="(request-target) host date digest",signature="${signature}"`,
+      accept: "application/json",
+    },
+    body,
+  });
+
+  if (res.status < 200 || 299 < res.status) {
+    throw new Error(res.statusText + ": " + (await res.text()));
+  }
+
+  return res;
+}
 
 app.listen(3010)
