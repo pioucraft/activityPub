54cd05225a176db086a9f477f2840353c672eb11
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-20 18:42:03 +0200

    testing

--- index.js
+++ index.js
@@ -61,13 +61,13 @@ const data = [
     `digest: SHA-256=${digest}`,
     `host: mastodon.gougoule.ch`,
     `date: ${date}`,
-    `Content-Type: application/activity+json`
+    `content-type: application/activity+json`
   ].join("\n");
 console.log(data)
 const signature = sign("sha256", Buffer.from(data), key).toString("base64");
 
 console.log(signature)
-fetch("https://mastodon.gougoule.ch/users/pfannkuchen/inbox", {"method": "POST", headers: {"Date": date, "Content-Type": "application/activity+json", "Host": "mastodon.gougoule.ch", "Signature": `keyID="https://social.gougoule.ch/actor/#main-key",algorithm="rsa-sha256",headers="(request-target) host digest date content-type",signature="${signature}"`, "Digest": "SHA-256="+digest}, "body": JSON.stringify({"@context": "https://www.w3.org/ns/activitystreams", "id": activity_id, "type": "Follow", "actor": "https://social.gougoule.ch/actor", "object": "https://mastodon.gougoule.ch/users/pfannkuchen"})}).then(data => data).then(data => {
+fetch("https://mastodon.gougoule.ch/users/pfannkuchen/inbox", {"method": "POST", headers: {"Date": date, "content-type": "application/activity+json", "Host": "mastodon.gougoule.ch", "Signature": `keyID="https://social.gougoule.ch/actor/#main-key",algorithm="rsa-sha256",headers="(request-target) digest host date content-type",signature="${signature}"`, "Digest": "SHA-256="+digest}, "body": JSON.stringify({"@context": "https://www.w3.org/ns/activitystreams", "id": activity_id, "type": "Follow", "actor": "https://social.gougoule.ch/actor", "object": "https://mastodon.gougoule.ch/users/pfannkuchen"})}).then(data => data).then(data => {
 
     console.log(data)
     
