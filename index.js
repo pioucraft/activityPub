6fe13fab8aa808ee8c409012b8cfdb6d089e22f0
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-26 13:39:46 +0200

    testing

--- index.js
+++ index.js
@@ -46,8 +46,61 @@ app.all("/actor", (req, res) => {
 })
 
 app.all("/inbox", (req, res) => {
-    console.log(req)
+    console.log("REQUEST INCOMMING\n\n\nREQUEST INCOMMING\n\n\nREQUEST INCOMMING\n\n\nREQUEST INCOMMING\n\n\n")
+    console.log("req : "req)
+    console.log("body req : "+req.body)
     res.status(202)
+    var activity_id = "https://social.gougoule.ch/"+crypto.randomUUID()
+    const requestBody = JSON.stringify({
+      "@context": "https://www.w3.org/ns/activitystreams",
+      "id": activity_id,
+      "type": "Accept",
+      "actor": "https://social.gougoule.ch/actor",
+      "object": req.body
+    });
+    var aString = requestBody
+    const hash = createHash('sha256');
+    hash.update(aString, 'utf-8');
+    const digest = hash.digest('base64');
+
+    
+    let date = new Date().toUTCString()
+    const key = createPrivateKey(privateKey)
+    const data = [
+        `(request-target): post ${"/users/pfannkuchen/inbox"}`,
+        `digest: SHA-256=${digest}`,
+        `host: mastodon.gougoule.ch`,
+        `date: ${date}`,
+      ].join("\n");
+    console.log(data)
+    const signature = sign("sha256", Buffer.from(data), key).toString("base64");
+
+    console.log(signature)
+
+
+
+    const contentLength = Buffer.byteLength(requestBody, 'utf-8'); // Calculate the length of the request body
+
+    fetch("https://mastodon.gougoule.ch/users/pfannkuchen/inbox", {
+      method: "POST",
+      headers: {
+        "Date": date,
+        "content-type": "application/activity+json",
+        "Host": "mastodon.gougoule.ch",
+        "Signature": `keyId="https://social.gougoule.ch/actor/#main-key",algorithm="rsa-sha256",headers="(request-target) digest host date",signature="${signature}"`,
+        "accept": "application/json",
+        "Digest": `SHA-256=${digest}`,
+        "Content-Length": contentLength // Add the Content-Length header
+      },
+      body: requestBody
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+    console.error("Request error:", error);
+    });
+
 })
 
 var activity_id = "https://social.gougoule.ch/"+crypto.randomUUID()
